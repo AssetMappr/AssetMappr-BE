0a558741ef6301800c39af711ece8f773f5df47d
Niranjan Kumawat <nkumawat@andrew.cmu.edu>
2023-06-26 22:06:17 -0400

    Add more table creation

--- db_init/db_init.py
+++ db_init/db_init.py
@@ -67,7 +67,7 @@ def drop_and_create():
                    "last_name VARCHAR(255),"
                    "mobile VARCHAR(12),"
                    "com_name VARCHAR(255),"
-                   "com_id INT NOT NULL,"
+                   "com_id BIGINT NOT NULL,"
                    "dob DATE NOT NULL,"
                    "ethnicity VARCHAR(100),"
                    "race VARCHAR(50),"
@@ -83,7 +83,7 @@ def drop_and_create():
         "name VARCHAR(255) NOT NULL,"
         "type VARCHAR(15) CHECK(type IN ('Tangible', 'Intangible')),"
         "com_name VARCHAR(255) NOT NULL,"
-        "com_id INT NOT NULL,"
+        "com_id BIGINT NOT NULL,"
         "source_type INT NOT NULL,"
         "source_name VARCHAR(255),"
         "category VARCHAR(255) NOT NULL,"
@@ -98,9 +98,62 @@ def drop_and_create():
         "FOREIGN KEY (com_id) REFERENCES communities (id),"
         "FOREIGN KEY (category_id) REFERENCES asset_categories (id)"
         ");")
+    # Asset updates
+    queries.append("CREATE TABLE asset_updates ("
+                   "id BIGSERIAL PRIMARY KEY NOT NULL,"
+                   "asset_id BIGINT NOT NULL,"
+                   "com_id BIGINT NOT NULL,"
+                   "name VARCHAR(255),"
+                   "description TEXT,"
+                   "address TEXT,"
+                   "latitude DOUBLE PRECISION,"
+                   "longitude DOUBLE PRECISION,"
+                   "timestamp TIMESTAMPTZ NOT NULL,"
+                   "category VARCHAR(255),"
+                   "category_id INT,"
+                   "type INT NOT NULL CHECK(type IN (0, 1)),"
+                   "status INT NOT NULL"
+                   "FOREIGN KEY (asset_id) REFERENCES assets (id),"
+                   "FOREIGN KEY (com_id) REFERENCES communities (id)"
+                   ");")
+    # Asset ratings
+    queries.append("CREATE TABLE asset_ratings ("
+                   "id BIGSERIAL PRIMARY KEY NOT NULL,"
+                   "asset_id BIGINT NOT NULL,"
+                   "com_id BIGINT NOT NULL,"
+                   "user_id BIGINT NOT NULL,"
+                   "timestamp TIMESTAMPTZ NOT NULL,"
+                   "rating_scale SMALLINT NOT NULL,"
+                   "comments TEXT,"
+                   "value VARCHAR(255) NOT NULL,"
+                   "FOREIGN KEY (asset_id) REFERENCES assets (id),"
+                   "FOREIGN KEY (com_id) REFERENCES communities (id),"
+                   "FOREIGN KEY (user_id) REFERENCES users (id)"
+                   ");")
+    # Completed surveys
+    queries.append("CREATE TABLE completed_surveys ("
+                   "survey_id BIGINT NOT NULL,"
+                   "planner_id BIGINT NOT NULL,"
+                   "survey_name TEXT NOT NULL,"
+                   "survey_purpose TEXT NOT NULL,"
+                   "s_time TIMESTAMPTZ NOT NULL,"
+                   "e_time TIMESTAMPTZ NOT NULL,"
+                   "com_name VARCHAR(255) NOT NULL,"
+                   "report JSON NOT NULL,"
+                   "report_link TEXT"
+                   "responses_count INT NOT NULL,"
+                   "visibility INT NOT NULL DEFAULT 0,"
+                   "PRIMARY KEY (survey_id, planner_id)"
+                   ");")
+    # User surveys -- past surveys
+    queries.append("CREATE TABLE user_surveys ("
+                   "survey_id BIGINT NOT NULL,"
+                   "user_id BIGINT NOT NULL,"
+                   "status INT NOT NULL DEFAULT 0,"
+                   "PRIMARY KEY (survey_id, user_id)"
+                   ");")
 
     execute_queries(queries)
-
 
 def populate_data():
     # Operations
